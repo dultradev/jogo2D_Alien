3c2ae68d21cb08699f9b0b4922a0dcff0c238a47
Rian Dultra (PC) <rian.albert.dultra@gmail.com>
2026-04-09 01:43:33 -0300

    Códigos exemplo de teste para o projeto.

---
+++ tests/test_doubles.py
@@ -0,0 +1,6 @@
+from tests.doubles import StubSemDesconto
+from src.desconto import Pedido
+
+def test_pedido_com_stub():
+    pedido = Pedido(StubSemDesconto())
+    assert pedido.total(100) == 100
